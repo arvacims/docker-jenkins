a3a4c8a8b3a1e2482afd3da46f4daa6628e2b977
berg207 <justuslasse.berger@bertelsmann.de>
2018-07-06 17:44:33 +0200

    Install more plugins during runtime (from text file)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 set -e
 
-echo "Genarate JENKINS SSH KEY"
-source /usr/local/bin/generate_key.sh
-echo "start JENKINS"
-# if `docker run` first argument start with `--` the user is passing jenkins launcher arguments
+echo 'Generating SSH key ...'
+/usr/local/bin/generate_key.sh
+
+if [ -e '/usr/share/jenkins/ref/plugins.txt' ]; then
+    echo 'Installing plugins ...'
+    /usr/local/bin/install-plugins.sh < /usr/share/jenkins/ref/plugins.txt
+fi
+
+echo 'Starting Jenkins ...'
 if [[ $# -lt 1 ]] || [[ "$1" == "--"* ]]; then
     exec tini -- /usr/local/bin/jenkins.sh "$@"
 fi
